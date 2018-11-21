165dc6d174d60e05ba6d051ef82691c6c337599d
Nathan Sosnovske <nsosnov@microsoft.com>
2018-11-20 22:15:15 -0800

    Add rectangle sample

---
+++ rectangle/src/main.rs
@@ -0,0 +1,16 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    heigth: u32
+}
+
+fn main() {
+    let rect1 = Rectangle { width: 30, heigth: 50 };
+
+    println!("rect1 is {:#?}", rect1);
+    println!("The area of the rectangle is {}", area(&rect1));
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.heigth * rectangle.width
+}
